0214d5923a1d82f5f790ce519447ceb1a324b39b
Adam <sector-f@users.noreply.github.com>
2022-09-26 19:45:03 -0400

    Started adding configurability

--- failover.go
+++ failover.go
@@ -12,7 +12,11 @@ import (
 type Failover struct {
 	OnRecv func(p ProbeStats)
 
-	probes      []probe
+	pingFreqency time.Duration
+	privileged   bool
+	numSeconds   uint
+
+	probes      []Probe
 	statTracker map[string]*rb.RingBuffer // Map destination address to ring buffer
 	statCh      chan ProbeStats
 	mu          sync.Mutex
@@ -21,7 +25,7 @@ type Failover struct {
 func NewFailover() *Failover {
 	f := &Failover{
 		// Placeholder probe values
-		probes: []probe{
+		probes: []Probe{
 			{
 				Dst: "192.168.0.1",
 			},
@@ -37,8 +41,16 @@ func NewFailover() *Failover {
 		mu:          sync.Mutex{},
 	}
 
+	if f.pingFreqency == 0 {
+		f.pingFreqency = 1 * time.Second
+	}
+
+	if f.numSeconds == 0 {
+		f.numSeconds = 10
+	}
+
 	for _, probe := range f.probes {
-		f.statTracker[probe.Dst] = rb.New(10) // TODO: make value configurable
+		f.statTracker[probe.Dst] = rb.New(f.numSeconds)
 	}
 
 	return f
@@ -48,6 +60,7 @@ func (f *Failover) Run() {
 	for _, probe := range f.probes {
 		go func(src, dst string) {
 			for {
+				// TODO: resolve DNS once at creation and then use value here
 				pinger, err := probing.NewPinger(dst)
 				if err != nil {
 					log.Println(err)
@@ -56,8 +69,8 @@ func (f *Failover) Run() {
 
 				pinger.SetPrivileged(true)
 				pinger.Count = 1
-				pinger.Timeout = 1 * time.Second
-				timer := time.NewTimer(1 * time.Second)
+				pinger.Timeout = f.pingFreqency
+				timer := time.NewTimer(f.pingFreqency)
 				pinger.Run()
 
 				f.statCh <- ProbeStats{
@@ -85,7 +98,33 @@ func (f *Failover) Run() {
 	}
 }
 
-type probe struct {
+type Option func(f *Failover)
+
+func WithProbes(probes []Probe) Option {
+	return func(f *Failover) {
+		f.probes = probes
+	}
+}
+
+func WithPingFrequency(t time.Duration) Option {
+	return func(f *Failover) {
+		f.pingFreqency = t
+	}
+}
+
+func WithPrivileged(p bool) Option {
+	return func(f *Failover) {
+		f.privileged = p
+	}
+}
+
+func WithNumSeconds(n uint) Option {
+	return func(f *Failover) {
+		f.numSeconds = n
+	}
+}
+
+type Probe struct {
 	Src string
 	Dst string
 }
